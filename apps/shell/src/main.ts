ea1d6f72e32ae7fe42d2b0c832f7ee580be0adc3
Jost <SeniorWebTechnologySpecialist@gmail.com>
2026-02-23 16:34:07 +0100

    zoneless

--- apps/shell/src/main.ts
+++ apps/shell/src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { AppComponent } from './app/app.component';
@@ -7,7 +7,7 @@ import { APP_ROUTES } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideRouter(APP_ROUTES),
   ],
 }).catch((err) => console.error(err));
