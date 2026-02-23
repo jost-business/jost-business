9a6522fa60c4240e828a8468ac42b2a4164449d6
Jost <SeniorWebTechnologySpecialist@gmail.com>
2026-02-23 23:29:02 +0100

    shell

--- apps/shell/src/app/app.component.ts
+++ apps/shell/src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -1,21 +1,3 @@
 import { Route } from '@angular/router';
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-home',
-  standalone: true,
-  template: `
-    <div>
-      <h1>Welcome to Jost Business</h1>
-      <p>Navigate to the different apps using the menu above.</p>
-    </div>
-  `,
-})
-class HomeComponent {}
-
-export const APP_ROUTES: Route[] = [
-  {
-    path: '',
-    component: HomeComponent,
-  },
-];
+export const APP_ROUTES: Route[] = [];
