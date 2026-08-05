0fb6203577f51fa3c70ac27bc58be29c84f6d36a
Jost <SeniorWebTechnologySpecialist@gmail.com>
2026-08-05 21:08:24 +0200

    fix: add CORS plugin to allow requests from countdown.jost.business

---
+++ apps/api/src/app/plugins/cors.ts
@@ -0,0 +1,10 @@
+import { FastifyInstance } from 'fastify';
+import fp from 'fastify-plugin';
+import cors from '@fastify/cors';
+
+export default fp(async function (fastify: FastifyInstance) {
+  fastify.register(cors, {
+    origin: ['https://countdown.jost.business', 'https://jost.business'],
+    methods: ['GET', 'POST'],
+  });
+});
